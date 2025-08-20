2167e4d6bb8030fdacab3f144081768adb53344f
sandeep-ai-hub <sandeephzb44@gmail.com>
2025-08-20 11:07:27 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum{ 
+    public static void add(){
+        int a=100;
+        int b=200;
+        int c= a+b;
+        System.out.println("The sum of "+ a +" and "+ b +" is:- "+c);
+    } 
+ public static void main(String args[]){  
+    add();
+ }  
+}  
